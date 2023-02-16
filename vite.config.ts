aa50950552d394926dd39a9ed6459ba70e1100a6
Iván Lo Giudice <ivanmaxlogiudice@gmail.com>
2023-02-16 01:43:53 -0300

    fix: wrong filename in build

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'iui',
+            fileName: 'iui',
         },
 
         /**
