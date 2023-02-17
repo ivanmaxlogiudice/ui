98f59fc64b6708a48b2f9c1111d1e3cc6c1a77e1
Iván Lo Giudice <ivanmaxlogiudice@gmail.com>
2023-02-17 09:59:57 -0300

    chore(docs): add all button's variants examples

--- docs/components/demos/IButton/DemoButtonVariants.vue
+++ docs/components/demos/IButton/DemoButtonVariants.vue
@@ -1,8 +1,39 @@
 <template>
-    <div class="flex items-center space-x-2">
-        <IButton variant="solid" color="primary">Solid</IButton>
-        <IButton variant="outline" color="primary">Outline</IButton>
-        <IButton variant="text" color="primary">Text</IButton>
-        <IButton variant="link" color="primary">Link</IButton>
+    <div class="space-y-2">
+        <div class="flex items-center space-x-2">
+            <IButton variant="solid" color="default">Default</IButton>
+            <IButton variant="solid" color="primary">Primary</IButton>
+            <IButton variant="solid" color="success">Success</IButton>
+            <IButton variant="solid" color="info">Info</IButton>
+            <IButton variant="solid" color="warning">Warning</IButton>
+            <IButton variant="solid" color="danger">Danger</IButton>
+        </div>
+
+        <div class="flex items-center space-x-2">
+            <IButton variant="outline" color="default">Default</IButton>
+            <IButton variant="outline" color="primary">Primary</IButton>
+            <IButton variant="outline" color="success">Success</IButton>
+            <IButton variant="outline" color="info">Info</IButton>
+            <IButton variant="outline" color="warning">Warning</IButton>
+            <IButton variant="outline" color="danger">Danger</IButton>
+        </div>
+
+        <div class="flex items-center space-x-2">
+            <IButton variant="text" color="default">Default</IButton>
+            <IButton variant="text" color="primary">Primary</IButton>
+            <IButton variant="text" color="success">Success</IButton>
+            <IButton variant="text" color="info">Info</IButton>
+            <IButton variant="text" color="warning">Warning</IButton>
+            <IButton variant="text" color="danger">Danger</IButton>
+        </div>
+
+        <div class="flex items-center space-x-2">
+            <IButton variant="link" color="default">Default</IButton>
+            <IButton variant="link" color="primary">Primary</IButton>
+            <IButton variant="link" color="success">Success</IButton>
+            <IButton variant="link" color="info">Info</IButton>
+            <IButton variant="link" color="warning">Warning</IButton>
+            <IButton variant="link" color="danger">Danger</IButton>
+        </div>
     </div>
 </template>
